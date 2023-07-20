5bb5c67bbd22c54fa5310f0d0de0109a69b9f5c3
Daniel Sanchez <morkendi@gmail.com>
2023-07-19 21:16:27 -0600

    User Routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,4 +9,15 @@ const {
     deleteUser,
     updateFriend,
     deleteFriend
-} = require('../../controllers/userController')
+} = require('../../controllers/userController');
+
+// Users GET all & POST
+router.route('/').get(getAllUsers).post(createUser);
+
+// Users GET single, PUT & DELETE
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// Friends PUT & DELETE
+router.route('/:id/friends/:friendId').post(updateFriend).delete(deleteFriend);
+
+module.exports = router;
